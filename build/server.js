b702e5ec746b8449cd1be0b0e78feadf8c1e9714
Ibranista <ibranista88@gmail.com>
2023-03-04 01:49:00 +0100

    install cors.

--- build/server.js
+++ build/server.js
@@ -27,6 +27,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const dotenv = __importStar(require("dotenv"));
 const db_js_1 = __importDefault(require("./config/db.js"));
 const songRoutes_js_1 = __importDefault(require("./routes/songRoutes.js"));
@@ -34,6 +35,7 @@ dotenv.config();
 const PORT = process.env.PORT || 7000;
 (0, db_js_1.default)();
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
 app.listen(PORT, () => console.log(`running on port ${PORT}`));
 console.log("port: ", process.env.MONGO_URI);
 // middlewares
