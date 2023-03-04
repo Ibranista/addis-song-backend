878dd67372352979f1461e7334ff6271942cff2f
Ibranista <ibranista88@gmail.com>
2023-03-04 01:42:22 +0100

    use cors.

--- server.ts
+++ server.ts
@@ -1,18 +1,25 @@
 import express from "express";
+import cors from "cors";
 import * as dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import songRoutes from "./routes/songRoutes.js";
+
 dotenv.config();
 const PORT = process.env.PORT || 7000;
 connectDB();
 const app = express();
+
+app.use(cors());
+
 app.listen(PORT, () => console.log(`running on port ${PORT}`));
 console.log("port: ", process.env.MONGO_URI);
-// middlewares
 
+// middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
   res.send("port is working");
 });
+
 app.use("/songs", songRoutes);
